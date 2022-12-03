04e82174b14d8d39682fa2d31e291847c892e462
Danik <voddden@gmail.com>
2022-12-03 23:31:13 +0300

    сейв

--- Lab-5/Lab-5__task2/Lab-5__task2.cpp
+++ Lab-5/Lab-5__task2/Lab-5__task2.cpp
@@ -1,6 +1,8 @@
 #include "Validation.h"
 
-/* Заполнить массив с клавиатуры. Удалить все элементы, кратные 11.*/
+/* Двумерный массив заполнить числами.
+ * Количество чисел в строке может быть различным, последнее число - 0.
+ * В каждой строке удалить все максимальные элементы */
 
 int** allocation(int m, int n)
 {
@@ -16,6 +18,17 @@ void inputArr(int** arr, int m, int n) {
     char iHateC[] = "arr[.][.] =";
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
+            iHateC[4] = i + 48;
+            iHateC[7] = j + 48;
+            arr[i][j] = inputInteger(iHateC);
+        }
+    }
+}
+// input of teeth`ed arrays
+void inputModified(int** arr, int m, int* N){
+    char iHateC[] = "arr[.][.] =";
+    for (int i = 0; i < m; ++i){
+        for (int j = 0; j < N[i]; ++j){
             iHateC[4] = i + 48;
             iHateC[7] = j + 48;
             arr[i][j] = inputInteger(iHateC);
@@ -51,27 +64,32 @@ void Free(int** arr, int m) {
     free(arr);
 }
 
-// operate
+
+
 void deleteElement(int* mas, int& length, int index) {
     for(int i = index; i < length - 1; ++i)
         mas[i] = mas[i + 1];
     mas = (int*)realloc(mas, --length);
 }
 
-void operate(int** arr, int m, int* N) {
-    for (int i = 0; i < m; ++i){
-        for (int j = 0; j < N[i]; ++j){
-            if (arr[i][j] % 11 == 0 && arr[i][j] != 0) {
-                deleteElement(arr[i], N[i], j);
-                --j;
-            }
-        }
+int indexOfMax(int* mas, int length){
+    int s = mas[0];
+    for (int i = 1; i < length; ++i){
+        if (s < mas[i])
+            s = mas[i];
     }
+    return s;
+}
+
+// преобразование массива:
+void operate(int** arr, int m, int* N) {
+    for (int i = 0; i < m; ++i)
+        deleteElement(arr[i], N[i], indexOfMax(arr[i], N[i]));
 }
 
 int main() {
     srand(time(NULL));
-    printf("Lab 5, task 1\n\n");
+    printf("Lab 5, task 2\n\n");
     printf("Matrix m x n\n");
 
     int m = inputNatural("m =");
@@ -82,24 +100,26 @@ int main() {
 
     int** arr = allocation(m, n);
 
-    int choice = inputNatural("---input matrix from keyboard - 1\t\trandom - 2\n");
-    while (choice != 1 && choice != 2) {
-        choice = inputNatural("---input matrix from keyboard - 1\t\trandom - 2\n");
+    int choice = inputNatural("---input matrix from keyboard - 1\t\trandom - 2\tteeth input - 3\n");
+    while (choice != 1 && choice != 2 && choice != 3) {
+        choice = inputNatural("---input matrix from keyboard - 1\t\trandom - 2\tteeth input - 3\n");
     }
 
     switch (choice) {
         case 1:
             inputArr(arr, m, n); break;
         case 2:
-            randArr(arr, m, n);
+            randArr(arr, m, n); break;
+        case 3:
+            inputModified(arr, m, N);
     }
 
     // вывод первоначального массива
     printf("\nInitial matrix:\n\n");
-    outputArr(arr, m, n);
+    outputModified(arr, m, N);
 
-    // преобразование массива: (удаление элементов, кратных 11)
-    operate(arr, m, N);
+    // преобразование массива: (удаление максимальных элементов в каждой из строк)
+    //operate(arr, m, N);
 
     // вывод преобразованного массива:
     printf("\nFinal matrix:\n\n");
