56062f9090403270ea61f54ee821d7bf0449a204
Danik <voddden@gmail.com>
2022-12-08 14:50:40 +0300

    added debug file of lab7 to gitignore

--- Lab-7/Lab-7__task2/main.cpp
+++ Lab-7/Lab-7__task2/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+/* Дан символ C и строка S. Удалить каждое второе вхождение символа C в строку S */
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
