5208162292d4975ffa84dfdbdd8ee816f009f382
Danik <voddden@gmail.com>
2023-02-11 13:06:17 +0300

    save

--- Lab-1/Lab1__task1/Lab1__task1.cpp
+++ Lab-1/Lab1__task1/Lab1__task1.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+
+/* Вариант 7
+1. С клавиатуры заполнить файл целыми числами.
+*/
 
 int main() {
-    
+
+    char* message = (char*)malloc(100);
+    puts("Enter integer:");
+    gets(message);
+    int length = sizeof (message);
+
+    FILE* filePointer = fopen("D://data.txt", "w");
+    if (filePointer == NULL) {
+        perror("Error! No file found");
+        return 1;
+    }
+
+    int temp;
+    for (int i = 0; i < length; ++i) {
+        putc(message[i], filePointer);
+    }
+    fclose(filePointer);
+    printf("File has been written");
+    return 0;
 }
